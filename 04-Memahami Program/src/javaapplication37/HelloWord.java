df5745c5e19b978aace4fba96794beb79d5a8cca
rikymuhamadzein <rikymcesar@gmail.com>
2019-12-12 12:39:52 +0700

    Java_Pertemuan13

---
+++ 04-Memahami Program/src/javaapplication37/HelloWord.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication37;
+/**
+     * Nama : Inar
+     * NIM  : A2.190082
+     * Kelas: TI-A
+     */
+public class HelloWord {
+
+ 
+    public static void main(String[] args) {
+    System.out.println("hello world");
+    }
+    
+}
